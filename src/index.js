58d59d810a930b0e9ee642642acb9101006f6c91
Renjith <grenjithr@gmail.com>
2018-08-28 12:58:43 +0200

    use const instead of var

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { render } from 'react-dom';
 import React from 'react';
 import CreditCard from './CreditCard';
 
-var mountNode = document.getElementById("app");
+const mountNode = document.getElementById("app");
 
 render(
     <div className='demo-container'><CreditCard /></div>,
